ceb1d4810bdbd5b37bbcaa45cfa05b43b5bbfc2d
MELİKE GÜN <melike.gun1@ogr.sakarya.edu.tr>
2025-03-08 19:13:18 +0300

    commit7

---
+++ Eticaret.WebUI/Controllers/CategoriesController.cs
@@ -0,0 +1,29 @@
+﻿using Eticaret.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Eticaret.WebUI.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly DatabaseContext _context;
+
+        public CategoriesController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index(int? id)
+        {
+            if(id == null)
+            {
+               return NotFound();
+            }
+
+            var category = await _context.Categories.Include(p => p.Products)
+                .FirstOrDefaultAsync(c => c.Id == id);
+
+            return View(category);
+        }
+    }
+}
---
+++ Eticaret.WebUI/Models/ProductDetailViwModel.cs
@@ -0,0 +1,10 @@
+﻿using Eticaret.Core.Entities;
+
+namespace Eticaret.WebUI.Models
+{
+    public class ProductDetailViwModel
+    {
+        public Product? Product { get; set; }
+        public IEnumerable<Product>? RelatedProducts { get; set; }
+    }
+}
